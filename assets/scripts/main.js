3f80f80231b3a46e478ee938fa50e1ca11299610
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-03-12 17:48:18 -0300

    sync assets com o repo landing

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,25 @@
    'use strict';
 
     $(function () {
-        console.log('oi');
+        var frm = $('#mailing');
+        frm.submit(function () {
+            $.ajax({
+                type: frm.attr('method'),
+                url: frm.attr('action'),
+                data: frm.serialize(),
+                success: function (data) {
+                    console.log(data);
+                    if (data.success) {
+                        $('.subscribe-form').hide();
+                        $('.subscribe-success').show();
+                        console.log('vai');
+                    }
+                },
+                error: function(data) {
+                    console.log(data);
+                }
+            });
+            return false;
+        });
     });
 })();
