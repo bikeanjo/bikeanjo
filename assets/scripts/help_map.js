986b26c2ffa715ec933d0a86acdaf3a926ab0c53
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-04-01 20:23:34 -0300

    submete json vindo do leaflet

--- assets/scripts/help_map.js
+++ assets/scripts/help_map.js
@@ -1,7 +1,19 @@
 jQuery(function(){
+    var __log = console.log.bind(console);
     // load apis
-    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    if($("#js-map").length !== 1) {
+        return;
+    }
+
+    var tempMarker = null;
+    var map = L.map('js-map').setView([-23.548991, -46.633328], 13);
     var geocoder = new google.maps.Geocoder();
+    var $list = $("#js-addresses");
+    var $addBtn = $("#js-add-address");
+    var $geocoderInput = $('#geocoder-input');
+    var $jsonPointsInput = $("#id_json_points");
+    var points = [ ];
 
     // setup leaflet
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
@@ -12,13 +24,6 @@ jQuery(function(){
         id: 'examples.map-i875mjb7'
     }).addTo(map);
 
-    function placeOnMap(address) {
-        L.marker([
-            address.geometry.location.lat(), 
-            address.geometry.location.lng(), 
-        ]).addTo(map)
-          .bindPopup(address.formatted_address).openPopup();
-    }
 
     // basic geoCodeAddress
     function geoCodeAddress(address) {
@@ -58,17 +63,67 @@ jQuery(function(){
         return addresses[0];
     }
 
-    // setup elements
-    var $geocoderInput = $('#geocoder-input');
+    function appendOnList(marker) {
+        var $li = $('<li>').text(marker.address.formatted_address);
+        $li.appendTo($list);
+    }
+
+    function addMarker(address) {
+        var marker = L.marker([
+            address.geometry.location.lat(), 
+            address.geometry.location.lng(), 
+        ]);
+        marker.bindPopup(address.formatted_address);
+        marker.addTo(map);
+        marker.address = address;
+
+        return marker;
+    }
+
+    function addTempMarker(address) {
+        tempMarker = addMarker(address);
+        return tempMarker;
+    }
+
+    function removeTempMarker() {
+        if (tempMarker) {
+            map.removeLayer(tempMarker);
+        }
+    }
+
+    // setup elements and events
+    $addBtn.click(function(){
+        if(tempMarker) {
+            points.push(tempMarker);
+            appendOnList(tempMarker);
+            tempMarker = null;
+        }
+        $geocoderInput.val('');
+        $geocoderInput.focus();
+        $geocoderInput.click();
+        return false;
+    });
+
     $geocoderInput.keydown(function(e){
         if(e.which === 13) {
+            removeTempMarker();
             var address = $(this).val();
             geoCodeAddress(address)
                 .then(selectCorrectAddress)
-                .then(placeOnMap);
-
+                .then(addTempMarker)
+                .then(__log);
             return false;
         }
     });
 
+    $('form').submit(function() {
+        var json = points.map(function(marker){
+            return {
+                'address': marker.address.formatted_address,
+                'lat': marker.address.geometry.location.lat(),
+                'lon': marker.address.geometry.location.lng()
+            };
+        });
+        $jsonPointsInput.val(JSON.stringify(json));
+    });
 });
