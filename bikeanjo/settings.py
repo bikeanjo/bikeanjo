1a34b48f3d50ea1cce2edfcf301b262323dd3fd9
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-05-22 10:46:36 -0300

    configura settings para i18n

--- bikeanjo/settings.py
+++ bikeanjo/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,6 +121,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 
 
 # Static files (CSS, JavaScript, Images)
