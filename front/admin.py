291900f357e07db0671a29aa5ab7ab468dafd785
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-05-22 18:32:06 -0300

    mais customizações do django admin

--- front/admin.py
+++ front/admin.py
@@ -1,5 +1,8 @@
+from urllib import urlencode
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
 from front import models
 
@@ -11,10 +14,11 @@ admin.site.index_title = _('Site administration')
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
     list_display = ('full_name', 'is_active', 'city', 'role', 'active_requests',
-                    'finalized_requests', 'service_rating')
+                    'finalized_requests', 'service_rating', 'tracks', 'points')
 
     def full_name(self, obj):
         return obj.get_full_name() or obj.username
+    full_name.short_description = _('Full name')
 
     def active_requests(self, obj):
         counter = '-'
@@ -22,6 +26,20 @@ class CustomUserAdmin(UserAdmin):
             counter = obj.helpbikeanjo_set.active().count()
         elif obj.role == 'requester':
             counter = obj.helprequested_set.active().count()
+
+        if counter > 0:
+            query = {
+                obj.role: obj.id,
+                'status__in': 'new,open',
+            }
+            return format_html(
+                '{} <small><a href="{}?{}">{}</a></small>',
+                counter,
+                reverse('admin:front_helprequest_changelist'),
+                urlencode(query),
+                _('List')
+            )
+
         return counter
     active_requests.short_description = _('Active requests')
 
@@ -31,6 +49,19 @@ class CustomUserAdmin(UserAdmin):
             counter = obj.helpbikeanjo_set.filter(status='finalized').count()
         elif obj.role == 'requester':
             counter = obj.helprequested_set.filter(status='finalized').count()
+        if counter > 0:
+            query = {
+                obj.role: obj.id,
+                'status__in': 'finalized',
+            }
+            return format_html(
+                '{} <small><a href="{}?{}">{}</a></small>',
+                counter,
+                reverse('admin:front_helprequest_changelist'),
+                urlencode(query),
+                _('List')
+            )
+
         return counter
     finalized_requests.short_description = _('Finalized requests')
 
@@ -44,10 +75,55 @@ class CustomUserAdmin(UserAdmin):
         return rating or 0
     service_rating.short_description = _('Service rating')
 
+    def tracks(self, obj):
+        counter = obj.track_set.count()
+        if counter > 0:
+            query = {'user': obj.id}
+            return format_html(
+                '{} <small><a href="{}?{}">{}</a></small>',
+                counter,
+                reverse('admin:front_track_changelist'),
+                urlencode(query),
+                _('List')
+            )
+        return counter
+    tracks.short_description = _('Tracks')
+
+    def points(self, obj):
+        counter = obj.point_set.count()
+        if counter > 0:
+            query = {'user': obj.id}
+            return format_html(
+                '{} <small><a href="{}?{}">{}</a></small>',
+                counter,
+                reverse('admin:front_point_changelist'),
+                urlencode(query),
+                _('List')
+            )
+        return counter
+    points.short_description = _('Points')
+
+    def lookup_allowed(self, lookup, value):
+        allow = [
+            'contentreadlog__object_id',
+            'contentreadlog__content_type__model',
+            'contentreadlog__content_type__app_label',
+        ]
+
+        if lookup in allow:
+            return True
+
+        return super(CustomUserAdmin, self).lookup_allowed(lookup, value)
+
 
 @admin.register(models.Track)
 class TrackAdmin(admin.ModelAdmin):
     list_display = ('user', 'start', 'end', 'track',)
+
+
+@admin.register(models.Point)
+class PointAdmin(admin.ModelAdmin):
+    list_display = ('address', 'coords',)
 
 
 class HelpReplyInline(admin.TabularInline):
@@ -60,16 +136,23 @@ class HelpReplyInline(admin.TabularInline):
 @admin.register(models.HelpRequest)
 class HelpRequestAdmin(admin.ModelAdmin):
     inlines = [HelpReplyInline]
-    list_display = ('requester_name', 'bikeanjo_name', 'get_help_label',
+    search_fields = ('requester_name', 'bikeanjo_name',)
+    list_display = ('requester_name', 'bikeanjo_name', 'get_help_label_',
                     'status', 'requester_rating', 'requester_eval',)
 
+    def get_help_label_(self, obj):
+        return obj.get_help_label()
+    get_help_label_.short_description = _('Help with')
+
     def requester_name(self, obj):
-        return obj.requester.get_full_name()
+        return obj.requester.get_full_name() or obj.requester.username
+    requester_name.short_description = _('Requester name')
 
     def bikeanjo_name(self, obj):
         if obj.bikeanjo is None:
             return ''
         return obj.bikeanjo.get_full_name()
+    bikeanjo_name.short_description = _('Bikeanjo name')
 
 
 @admin.register(models.Message)
@@ -78,7 +161,21 @@ class MessageAdmin(admin.ModelAdmin):
     search_fields = ('title',)
 
     def readed_by_(self, obj):
-        return obj.readed_by.count()
+        counter = obj.readed_by.count()
+        if counter > 0:
+            query = {
+                'contentreadlog__object_id': obj.id,
+                'contentreadlog__content_type__model': obj._meta.model_name,
+                'contentreadlog__content_type__app_label': obj._meta.app_label,
+            }
+            return format_html(
+                '{} <small><a href="{}?{}">{}</a></small>',
+                counter,
+                reverse('admin:front_user_changelist'),
+                urlencode(query),
+                _('List')
+            )
+        return counter
     readed_by_.short_description = _('Readed by')
 
 
