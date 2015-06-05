dff56739142bdd5642bd5b57ab7bb44add1879b7
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-06-05 15:19:47 -0300

    faz enviar emails quando usuario envia mensagem sobre o pedido de ajuda

--- front/signals.py
+++ front/signals.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import select_template
 from django.utils.timezone import now
 from front import models
 
@@ -33,3 +37,37 @@ def assign_bike_anjo(sender, instance, **kwargs):
             helprequest=instance,
             score=score,
         )
+
+
+@receiver(post_save, sender=models.HelpReply)
+def notify_new_reply_by_email(sender, instance, **kwargs):
+    helprequest = instance.helprequest
+
+    recipient = None
+    if instance.author != helprequest.requester:
+        recipient = helprequest.requester
+    elif helprequest.bikeanjo:
+        recipient = helprequest.bikeanjo
+
+    if not recipient:
+        return
+
+    site = Site.objects.filter(id=settings.SITE_ID).first()
+    subject = 'Você recebeu uma nova mensagem'
+    from_email = settings.DEFAULT_FROM_EMAIL
+    data = {
+        'helprequest': helprequest,
+        'recipient': recipient,
+        'sender': instance.author,
+        'site': site,
+    }
+
+    template_name = 'emails/new_msg_to_%s.html' % recipient.role
+    html = select_template([template_name]).render(data)
+
+    template_name = 'emails/new_msg_to_%s.txt' % recipient.role
+    text = select_template([template_name]).render(data)
+
+    msg = EmailMultiAlternatives(subject, text, from_email, [recipient.email])
+    msg.attach_alternative(html, "text/html")
+    msg.send()
