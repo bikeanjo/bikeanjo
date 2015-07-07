ba8a948a278e9e91a1dd70879e0007e5cc6bcd93
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-07-07 19:15:33 -0300

    setup the auto-header and load flatpages from templates

---
+++ front/migrations/0025_auto_20150707_1900.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import re
+from django.conf import settings
+from django.db import models, migrations
+
+
+def load_flatpages_from_template(apps, schema_editor):
+    Site = apps.get_model('sites', 'Site')
+    pattern = re.compile(r'{% *block *content *%}([^{]+){% *endblock *%}', re.MULTILINE)
+
+    content = open(os.path.join(settings.BASE_DIR, 'templates/about.html')).read()
+    content = pattern.search(content).group(1)
+    Site.objects.first().flatpage_set.create(
+        title='Sobre nós',
+        url='/about/sobre-nos/',
+        content=content,
+    )
+
+    content = open(os.path.join(settings.BASE_DIR, 'templates/how_it_works.html')).read()
+    content = pattern.search(content).group(1)
+    Site.objects.first().flatpage_set.create(
+        title='Como funciona',
+        url='/work/como-funciona/',
+        content=content,
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('front', '0024_auto_20150706_2159'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_flatpages_from_template)
+    ]
