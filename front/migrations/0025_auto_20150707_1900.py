1ddebe71de4ab5456792f32b4f44bdccfbd59547
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-08-11 17:14:51 -0300

    fix migrations

--- front/migrations/0025_auto_20150707_1900.py
+++ front/migrations/0025_auto_20150707_1900.py
@@ -12,6 +12,11 @@ def load_flatpages_from_template(apps, schema_editor):
 
     content = open(os.path.join(settings.BASE_DIR, 'templates/about.html')).read()
     content = pattern.search(content).group(1)
+
+    site = Site.objects.first()
+    if not site:
+        site = Site.objects.create(name='bikeanjo.org', domain='bikeanjo.org')
+
     Site.objects.first().flatpage_set.create(
         title='Sobre nós',
         url='/about/sobre-nos/',
