8aa481027ff16ef309d03e79ca64aeb4d45a95ba
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-09-10 01:22:08 -0300

    change default format for django admin

---
+++ bikeanjo/formats/pt_BR/formats.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+ 
+DATE_FORMAT = 'd/m/Y'
+DATETIME_FORMAT = 'd/m/Y, H:i'
--- front/admin_resources.py
+++ front/admin_resources.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from import_export import resources, fields
 from front import models
 from cyclists.models import User
