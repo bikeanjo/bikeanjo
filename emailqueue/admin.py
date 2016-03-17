d821c355c17913bfc1eb63e007fb8b3bac8236e1
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2016-03-17 19:12:44 -0300

    add simple admin entry for mail queue

--- emailqueue/admin.py
+++ emailqueue/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from emailqueue.models import QueuedMail
 
-# Register your models here.
+
+@admin.register(QueuedMail)
+class QueuedMailAdmin(admin.ModelAdmin):
+    list_filter = ('date', 'tag', 'sent',)
+    list_display = ('to', 'sender', 'date', 'tag', 'sent', 'subject', 'errors',)
+    search_fields = ('local', 'relato', 'agente', 'projeto', 'linha_estrategica')
