8bfb137928b7e79b5aabfa4c61c79704a07703d7
Dimitri Bello <o273227@mvrht.com>
2017-08-13 23:52:50 -0300

    filtro de ordenação por datas

--- front/admin.py
+++ front/admin.py
@@ -10,7 +10,7 @@ from modeltranslation.admin import TranslationAdmin
 from dal import autocomplete
 
 from front import models
-from admin_filters import RequestStatusListFilter
+from admin_filters import CreatedDateListFilter, RequestStatusListFilter
 
 import admin_resources as resources
 
@@ -80,7 +80,7 @@ class HelpRequestAdmin(ImportExportModelAdmin):
                      'message',)
     list_display = ('created_date', 'requester_name', 'bikeanjo_name', 'get_help_label_',
                     'status', 'requester_rating', 'requester_eval',)
-    list_filter = ('created_date', RequestStatusListFilter, 'requester_rating',)
+    list_filter = (CreatedDateListFilter, RequestStatusListFilter, 'closed_by', 'requester_rating',)
 
     def get_help_label_(self, obj):
         return obj.get_help_label()
--- front/admin_filters.py
+++ front/admin_filters.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+import datetime
 from collections import OrderedDict
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
@@ -22,3 +24,47 @@ class RequestStatusListFilter(admin.SimpleListFilter):
         if value == '_noba':
             return queryset.filter(status='new', bikeanjo=None)
         return queryset
+
+
+class CreatedDateListFilter(admin.SimpleListFilter):
+    title = _('Date')
+    parameter_name = 'created_date'
+    template = 'admin/created_date_filter.html'
+    regex = re.compile('^(?P<day>\d\d)/(?P<month>\d\d)/(?P<year>20\d\d)$')
+
+    def lookups(self, request, model_admin):
+        return (('datepicker', self.value()),)
+
+    def choices(self, *args):
+        choice = {
+            'display': u'datepicker',
+            'name': 'created_date',
+            'query_string': '?created_date=%s&q=' % self.value(),
+            'selected': True,
+            'val': self.parse(self.value())
+        }
+        return [choice]
+
+    def parse(self, request):
+        value = self.value()
+        result = []
+        for date in value.split(','):
+            match = self.regex.match(date)
+            if(match):
+                date = {k: int(v) for k, v in match.groupdict().items()}
+                date = datetime.date(**date)
+                result.append(date)
+
+        key = self.parameter_name
+        if len(result) == 1:
+            return {key: result[0]}
+        elif len(result) == 2:
+            return {
+                "%s__gte" % key: result[0],
+                "%s__lte" % key: result[1],
+            }
+        return {}
+
+    def queryset(self, request, queryset):
+        dates = self.parse(request)
+        return queryset.filter(**dates)
